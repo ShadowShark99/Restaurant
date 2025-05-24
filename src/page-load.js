f6f4c9d63fa30c6d9b07de5f1367f793bb08f045
ShadowShark99 <kadenwchan@gmail.com>
2025-05-23 22:02:51 -0500

    Refactor loading logic into page-load.js

---
+++ src/page-load.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import steve from "./mcSteve.png";
+
+export const load = () => {
+  
+
+const content = document.querySelector("#content");
+
+const image = document.createElement("img");
+image.src = steve;
+
+const headline = document.createElement("h2");
+headline.innerHTML = "Cheapest Food NOT Because Lowest Wages :D";
+
+const description = document.createElement("div");
+description.innerHTML = "All our food is made with original chicken from the first chicken that were created along with the world";
+
+content.appendChild(image);
+content.appendChild(headline);
+content.appendChild(description);
+alert("Restaurant is Opening soon");
+};
